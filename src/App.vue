85c235dc31092cb344b4b39e51f15a1e2236de2c
andresxaw <andresxaw@gmail.com>
2020-03-16 14:30:38 -0500

    coronavirus

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template lang="pug">
   .app
-    p Sebasián es gay
-  
+    div.sebas(@click="traer") Traer datos Covid-19 
+    div(v-for="caso in covidCases")
+      p Casos: {{ caso.cases }}
+      p Muertes: {{ caso.deaths }}
+      p Salvados: {{ caso.recovered }}
 </template>
 
 <script>
+const axios = require("axios").default;
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome, gay",
+      isName: false,
+      covidCases: []
+    };
+  },
+
+  methods: {
+    changeMsg() {
+      this.msg = "bye, gay";
+      this.isName = true;
+    },
+    async traer() {
+      const response = await axios.get('https://coronavirus-19-api.herokuapp.com/all')
+      this.covidCases = [response.data]
+      console.log(response)
     }
   }
-}
+};
 </script>
 
 <style lang="stylus">
+.app {
+  text-align: center;
+}
+
+.sebas
+  color #1b5a8f
+  display inline-block
+  font-size 30px
+  text-align center
+  background-color #4682b4
+  border-radius 50px
+  padding 15px
+
+.sebas:hover
+  background-color black
+  cursor pointer
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import babelPolyfill from 'babel-polyfill'
 
 new Vue({
   el: '#app',
